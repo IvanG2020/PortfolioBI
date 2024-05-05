41037b4aa8530aa501e176fa367aaa8c33f531ca
Ivan Galas <90492564+IvanG2020@users.noreply.github.com>
2024-05-04 23:56:48 -0700

    Add project files.

---
+++ PortfolioBI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortfolioBI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
