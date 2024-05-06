e61d6f8fc23958b102eb96d24f77d574c689efe5
Ivan Galas <90492564+IvanG2020@users.noreply.github.com>
2024-05-06 00:06:07 -0700

    Identify and report the most significant positive spike

--- PortfolioBI/Controllers/HomeController.cs
+++ PortfolioBI/Controllers/HomeController.cs
@@ -24,31 +24,86 @@ namespace PortfolioBI.Controllers
             DateTime endDate = new DateTime(2015, 6, 30);
 
             var glwData = _context.FinancialData
-                .Where(d => d.Ticker == "GLW" && d.Date >= startDate && d.Date <= endDate);
-            var nvdaData = _context.FinancialData
-                .Where(d => d.Ticker == "NVDA" && d.Date >= startDate && d.Date <= endDate);
+                .Where(d => d.Ticker == "GLW" && d.Date >= startDate && d.Date <= endDate)
+                .OrderBy(d => d.Date)
+                .ToList();
 
-            var stockSummary = new
+            var nvdaData = _context.FinancialData
+                .Where(d => d.Ticker == "NVDA" && d.Date >= startDate && d.Date <= endDate)
+                .OrderBy(d => d.Date)
+                .ToList();
+
+            var glwSummary = new StockSummary
             {
-                GLWMin = glwData.Min(d => d.Close),
-                GLWMax = glwData.Max(d => d.Close),
-                GLWAvg = glwData.Average(d => d.Close),
-                NVDAMin = nvdaData.Min(d => d.Close),
-                NVDAMax = nvdaData.Max(d => d.Close),
-                NVDAAvg = nvdaData.Average(d => d.Close)
+                Min = glwData.Min(d => d.Close),
+                Max = glwData.Max(d => d.Close),
+                Average = glwData.Average(d => d.Close)
             };
 
-            string stockSummaryJson = JsonConvert.SerializeObject(stockSummary);
+            var nvdaSummary = new StockSummary
+            {
+                Min = nvdaData.Min(d => d.Close),
+                Max = nvdaData.Max(d => d.Close),
+                Average = nvdaData.Average(d => d.Close)
+            };
 
-            return View(model: stockSummaryJson); // Pass the serialized JSON as a string
+            var glwSpike = FindSignificantSpike(glwData);
+            var nvdaSpike = FindSignificantSpike(nvdaData);
+
+            // Fetch the previous close price for GLW spike
+            var glwPreviousPrice = glwData.FirstOrDefault(d => d.Date < glwSpike.Date)?.Close;
+
+            // Fetch the previous close price for NVDA spike
+            var nvdaPreviousPrice = nvdaData.FirstOrDefault(d => d.Date < nvdaSpike.Date)?.Close;
+
+            var data = new
+            {
+                GLWSummary = glwSummary,
+                NVDASummary = nvdaSummary,
+                GLWSpike = new
+                {
+                    Date = glwSpike.Date,
+                    Close = glwSpike.Close,
+                    PreviousClose = glwPreviousPrice
+                },
+                NVDASpike = new
+                {
+                    Date = nvdaSpike.Date,
+                    Close = nvdaSpike.Close,
+                    PreviousClose = nvdaPreviousPrice
+                }
+            };
+
+            string jsonData = JsonConvert.SerializeObject(data);
+
+            return View(model: jsonData);
         }
 
-        /// <summary>
-        /// this method is used for uploading a CVS file with stock price data & inserting the data into our database table called FinancialData
-        /// for times sake I'm just uploading the data that is generated from Yahoo Finance and not coming up with a method to handle it including the ticker
-        /// i'm going to just manually insert the ticker price on the FinancialData model below 
-        /// data source: https://finance.yahoo.com/quote/NVDA/history?period1=1420070400&period2=1435622400
-        [HttpPost]
+        private FinancialData FindSignificantSpike(List<FinancialData> data)
+        {
+            FinancialData spike = null;
+            double maxIncrease = 0;
+
+            for (int i = 1; i < data.Count; i++)
+            {
+                double priceChange = data[i].Close - data[i - 1].Close;
+                if (priceChange > maxIncrease)
+                {
+                    maxIncrease = priceChange;
+                    spike = data[i];
+                }
+            }
+
+            return spike;
+        }
+
+
+    /// <summary>
+    /// this method is used for uploading a CVS file with stock price data & inserting the data into our database table called FinancialData
+    /// for times sake I'm just uploading the data that is generated from Yahoo Finance and not coming up with a method to handle it including the ticker
+    /// i'm going to just manually insert the ticker price on the FinancialData model below 
+    /// data source: https://finance.yahoo.com/quote/NVDA/history?period1=1420070400&period2=1435622400
+    [HttpPost]
         public IActionResult Upload()
         {
             try
---
+++ PortfolioBI/Models/CombinedViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace PortfolioBI.Models
+{
+    public class CombinedViewModel
+    {
+        public StockSummary GLWSummary { get; set; }
+        public StockSummary NVDASummary { get; set; }
+        public FinancialData GLWSpike { get; set; }
+        public FinancialData NVDASpike { get; set; }
+    }
+
+    public class StockSummary
+    {
+        public double Min { get; set; }
+        public double Max { get; set; }
+        public double Average { get; set; }
+    }
+}
